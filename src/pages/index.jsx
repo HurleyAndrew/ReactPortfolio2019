3017a0321fd2dfe8df12d98fe8a482c22902a0ac
Andrew Hurley <hurleymandrew@gmail.com>
2020-01-06 11:04:54 -0500

    updated og images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,8 @@ import '../layouts/index.js'
 import '../layouts/metaData.js'
 import ProjectNavigation from './../components/ProjectNavigation'
 import MetaTags from 'react-meta-tags'
+import Favicon from '../images/favicon.png'
+import Social from '../images/social.jpg'
 
 export default class IndexPage extends Component {
   _isMounted = false
@@ -45,6 +47,43 @@ export default class IndexPage extends Component {
       <div>
         <MetaTags>
           <title>Home</title>
+          <meta name="google" content="notranslate" />
+          <meta httpEquiv="Content-Language" content="en" />
+
+          <meta
+            name="description"
+            content="Portfolio Website for Andrew Hurley - Product Designer"
+          />
+
+          <meta property="og:title" content="Andrew Hurley" />
+          <meta
+            property="og:description"
+            content="Portfolio Website for Andrew Hurley - Product Designer"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content={Social} />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:site_name" content="https://hurleyandrew.com/" />
+          <meta property="og:url" content="https://hurleyandrew.com/" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+
+          <meta itemProp="name" content="Andrew Hurley Product Designer" />
+          <meta
+            itemProp="description"
+            content="Andrew Hurley Product Designer"
+          />
+          <meta itemProp="image" content="Andrew Hurley Product Designer" />
+
+          <meta name="author" content="Andrew Hurley" />
+
+          <link rel="shortcut icon" href={Favicon} type="image/x-icon" />
+          <link rel="icon" href={Favicon} type="image/x-icon" />
         </MetaTags>
 
         <div className="App">
